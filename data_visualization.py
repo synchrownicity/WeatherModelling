9e10ce87f036b6ddbc113ef7b6e0be97989426b6
lesstimeless <bassguitarhui123@gmail.com>
2025-03-15 12:19:18 +0800

    formed X and Y arrays

--- data_visualization.py
+++ data_visualization.py
@@ -1,7 +1,9 @@
+import pandas as pd
 
-print('hello')
+df = pd.read_csv("weather_data.csv")
+N = df.shape[0]
+feature_cols = df.shape[1]-1
 
-print(1)
-
-var1 = 3
+Y = df.iloc[:,-1]
+X = df.iloc[:, 0:feature_cols]
 
